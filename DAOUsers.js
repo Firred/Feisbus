42376a2b4bb4135699026e1681f7cb48a5a10320
Firred <adri-ankenobi@hotmail.com>
2019-12-11 16:44:50 +0100

    Fixed error in the update profile

--- DAOUsers.js
+++ DAOUsers.js
@@ -292,7 +292,7 @@ class DAOUsers {
                     params.push(user.email);
                 }
                 else {
-                    'WHERE email = ?;';
+                    sql += 'WHERE email = ?;';
                     params.push(user.email);
                 }
 
